ad5b710a0d983917be77976ee398f0948ec3d899
Erik Tomaselli <eriktomaselli97@gmail.com>
2023-09-25 19:28:02 -0300

    subiendo proyecto terminado , faltan detalles

---
+++ src/pages/CarritoPage.jsx
@@ -0,0 +1,64 @@
+import React, { useContext } from 'react'
+import { CarritoContext } from '../context/CarritoContext'
+
+export const CarritoPage = () => {
+
+    const { listaCompras, agregarCompra, aumentarCantidad, eliminarCompra, disminuirCantidad } = useContext(CarritoContext)
+
+    const calcularTotal = () => {
+        return listaCompras.reduce((total, item) => total + item.price * item.cantidad, 0).toFixed(2)
+    }
+
+    return (
+        <>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Item</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        listaCompras.map(item => (
+                            <tr key={item.id}>
+                                <th >{item.title} </th>
+                                <td>{item.price} </td>
+                                <td>
+                                    <button className='btn btn-outline-primary' onClick={() => disminuirCantidad(item.id)}>-</button>
+                                    <button className='btn btn-primary'>{item.cantidad} </button>
+                                    <button className='btn btn-outline-primary' onClick={() => aumentarCantidad(item.id)}>+</button>
+                                </td>
+                                <td>
+                                    <button
+                                        type='button'
+                                        className='btn btn-danger'
+                                        onClick={() => eliminarCompra(item.id)}
+                                    >
+                                        Eliminar
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+
+
+
+
+                </tbody>
+                <th><b>TOTAL: </b></th>
+                <td></td>
+                <td></td>
+                <td>${calcularTotal()} </td>
+            </table>
+
+            <div className="d-grid gap-2">
+                <button className='btn btn-primary' disabled={listaCompras < 1} >Finalizar Compra</button>
+            </div>
+
+        </>
+    )
+}
